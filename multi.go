bd17674fecc9be7d61ee834c2417a3b6c7b9cd51
godcong <jumbycc@163.com>
2020-08-12 17:39:21 +0800

    update io channel

--- multi.go
+++ multi.go
@@ -32,7 +32,7 @@ func (m *multiReader) Read(p []byte) (n int, err error) {
 			}
 		}
 		m.ctx, m.cancel = context.WithCancel(context.TODO())
-		m.count = atomic.NewInt32(int32(len(m.chanReader)))
+		m.count = atomic.NewInt32(int32(len(m.readers)))
 		m.chanReader = make(chan *chanReader, m.count.Load())
 		for i := 0; i < len(m.readers); i++ {
 			go func(ctx context.Context, cb chan<- *chanReader, index int, reader io.Reader) {
