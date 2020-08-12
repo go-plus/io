3db7e932e7ddae9bc0d0200d99b01064ac5e8770
godcong <jumbycc@163.com>
2020-08-12 15:18:16 +0800

    update io channel

--- multi.go
+++ multi.go
@@ -3,13 +3,14 @@ package io
 import (
 	"context"
 	"fmt"
+	"go.uber.org/atomic"
 	"io"
 )
 
 type multiReader struct {
 	ctx        context.Context
 	cancel     context.CancelFunc
-	count      int
+	count      *atomic.Int32
 	chanReader chan *chanReader
 	readers    []io.Reader
 }
@@ -30,8 +31,10 @@ func (m *multiReader) Read(p []byte) (n int, err error) {
 		}
 		m.ctx, m.cancel = context.WithCancel(context.TODO())
 		m.chanReader = make(chan *chanReader)
+		m.count = atomic.NewInt32(int32(len(m.chanReader)))
 		for i := 0; i < len(m.readers); i++ {
 			go func(ctx context.Context, cb chan<- *chanReader, index int, reader io.Reader) {
+				defer m.count.Dec()
 				for {
 					r := &chanReader{
 						index: index,
@@ -52,13 +55,7 @@ func (m *multiReader) Read(p []byte) (n int, err error) {
 	}
 
 	for {
-		if m.count <= 0 {
-			if m.cancel != nil {
-				m.cancel()
-				m.cancel = nil
-			}
-			close(m.chanReader)
-			m.chanReader = nil
+		if m.count.Load() <= 0 {
 			break
 		}
 		select {
@@ -70,19 +67,15 @@ func (m *multiReader) Read(p []byte) (n int, err error) {
 					m.cancel()
 					m.cancel = nil
 				}
-				close(m.chanReader)
-				m.chanReader = nil
 				return 0, fmt.Errorf("index(%d) was error:%w", r.index, r.err)
-			} else {
-				if r.err == io.EOF {
-					m.count--
-					continue
-				}
-				n := copy(p, r.p[:r.n])
-				return n, nil
+			} else if r.err == io.EOF {
+				continue
 			}
+			n := copy(p, r.p[:r.n])
+			return n, nil
 		}
 	}
+	close(m.chanReader)
 	return 0, io.EOF
 }
 
@@ -94,7 +87,6 @@ func MultiReader(readers ...io.Reader) io.Reader {
 	r := make([]io.Reader, len(readers))
 	copy(r, readers)
 	return &multiReader{
-		count:   len(r),
 		readers: r,
 	}
 }
